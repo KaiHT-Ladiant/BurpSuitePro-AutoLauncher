eab6fce8732607f982311cdfb147b4c3048af838
Kai_HT <157885033+KaiHT-Ladiant@users.noreply.github.com>
2025-03-06 11:42:04 +0900

    Update code.bat
    Update User typping Installed Path

--- code.bat
+++ code.bat
@@ -12,23 +12,25 @@ setlocal enabledelayedexpansion
 for /f %%W in ('powershell -Command "(Get-Host).UI.RawUI.WindowSize.Width"') do set "width=%%W"
 set "line="
 
-set "INSTALL_DIR=C:\Program Files\BurpSuitePro"
+REM set "INSTALL_DIR=C:\Program Files\BurpSuitePro"
+
+set /p INSTALL_DIR=Installed BurpsuitePro PATH: 
 set "JAVA_EXE=%INSTALL_DIR%\jre\bin\java.exe"
 set "LOADER_JAR=%INSTALL_DIR%\BurpLoaderKeygen_v1.17.jar"
 
 echo.
-echo ����������������������������������������������������������������������������������������������������������
-echo ��            BurpsuitePro Run with Keygen           ��
-echo ��               Scripting From Kai_HT               ��
-echo �Ʀ�������������������������������������������������������������������������������������������������������
+echo ¦£¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦¡¦Á
+echo ¦¢            BurpsuitePro Run with Keygen           ¦­
+echo ¦¢               Scripting From Kai_HT               ¦­
+echo ¦Æ¦¬¦¬¦¬¦¬¦¬¦¬¦¬¦¬¦¬¦¬¦¬¦¬¦¬¦¬¦¬¦¬¦¬¦¬¦¬¦¬¦¬¦¬¦¬¦¬¦¬¦¬¦¬¦¬¦¬¦¬¦¬¦¬¦¬¦¬¦¬¦¬¦¬¦¬¦¬¦¬¦¬¦¬¦¬¦¬¦¬¦¬¦¬¦¬¦¬¦¬¦¬¦°
 
 echo [+] Initializing BurpSuite Professional...
-for /l %%i in (1,1,%width%) do set "line=!line!��"
+for /l %%i in (1,1,%width%) do set "line=!line!¦¡"
 
-REM "C:\Program Files\BurpSuitePro\jre\bin\java.exe" "--add-opens=java.desktop/javax.swing=ALL-UNNAMED" "--add-opens=java.base/java.lang=ALL-UNNAMED" "--add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED" "--add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED" "--add-opens=java.base/jdk.internal.org.objectweb.asm.Opcodes=ALL-UNNAMED" "-javaagent:C:/Program Files/BurpSuitePro/BurpLoaderKeygen_v1.17.jar" "-noverify" "-jar" "C:\Program Files\BurpSuitePro\burpsuite_pro.jar" 
-java.exe "--add-opens=java.desktop/javax.swing=ALL-UNNAMED" "--add-opens=java.base/java.lang=ALL-UNNAMED" "--add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED" "--add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED" "--add-opens=java.base/jdk.internal.org.objectweb.asm.Opcodes=ALL-UNNAMED" "-javaagent:C:/Program Files/BurpSuitePro/BurpLoaderKeygen_v1.17.jar" "-noverify" "-jar" "C:\Program Files\BurpSuitePro\burpsuite_pro.jar" 
+"%INSTALL_DIR%\jre\bin\java.exe" "--add-opens=java.desktop/javax.swing=ALL-UNNAMED" "--add-opens=java.base/java.lang=ALL-UNNAMED" "--add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED" "--add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED" "--add-opens=java.base/jdk.internal.org.objectweb.asm.Opcodes=ALL-UNNAMED" "-javaagent:%INSTALL_DIR%/BurpLoaderKeygen_v1.17.jar" "-noverify" "-jar" "%INSTALL_DIR%\burpsuite_pro.jar" 
+REM java.exe "--add-opens=java.desktop/javax.swing=ALL-UNNAMED" "--add-opens=java.base/java.lang=ALL-UNNAMED" "--add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED" "--add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED" "--add-opens=java.base/jdk.internal.org.objectweb.asm.Opcodes=ALL-UNNAMED" "-javaagent:%INSTALL_DIR%/BurpLoaderKeygen_v1.17.jar" "-noverify" "-jar" "%INSTALL_DIR%\burpsuite_pro.jar" 
 
 
-for /l %%i in (1,1,%width%) do set "line=!line!��"
+for /l %%i in (1,1,%width%) do set "line=!line!¦¡"
 echo [+] Launch sequence completed
 pause
